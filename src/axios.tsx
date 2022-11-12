7cff9cfc21f696b2efdf81ba56a962f6f2bd8aa0
Joao Lucas NM <51080629+JoaoNM@users.noreply.github.com>
2022-11-12 11:13:52 +0000

    fixed base URL issue

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-	baseURL: ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:8080/api" : "https://oracui-v0.herokuapp.com/api"),
+	baseURL: ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:8000/" : "https://relivable-api-v0.herokuapp.com/"),
 	headers: {
 	},
 })
